0c0ad07e717e5c4e52b7e9bb875d5a54ddffa04c
fmarcos38 <fmarcos_23@hotmail.com>
2026-06-11 11:36:34 -0300

    deploy

--- src/Urls.js
+++ src/Urls.js
@@ -1,1 +1,6 @@
-export const URL = import.meta.env.VITE_API_URL || 'http://localhost:3001'
+export const URL_DESARROLLO =
+  import.meta.env.VITE_API_URL_DESARROLLO || import.meta.env.VITE_API_URL;
+export const URL_PRODUCCION =
+  import.meta.env.VITE_API_URL_PRODUCCION || import.meta.env.VITE_API_URL;
+
+export const URL = import.meta.env.PROD ? URL_PRODUCCION : URL_DESARROLLO;
